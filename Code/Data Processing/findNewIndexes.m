9b01f2e7ef359ebc35e5fe31b79906c9bdb35b18
Italo Belli <50029203+itbellix@users.noreply.github.com>
2023-07-11 17:34:26 +0200

    add files computing indexes. Might be useless but better to keep them

--- Code/Data Processing/findNewIndexes.m
+++ Code/Data Processing/findNewIndexes.m
@@ -8,38 +8,20 @@ mfile_name          = mfilename('fullpath');
 cd(pathstr);
 
 % Check whether the required timing matrix has already been computed
-if isfile('..\..\..\..\Results\new CMC analysis\timings_matrix_for_plotting_CMC_results.mat')
-     load('..\..\..\..\Results\new CMC analysis\timings_matrix_for_plotting_CMC_results.mat')
+if isfile('..\..\..\..\Results\CMC analysis\timings_matrix_for_plotting_CMC_results.mat')
+     load('..\..\..\..\Results\CMC analysis\timings_matrix_for_plotting_CMC_results.mat')
 else
      error("File corresponding to the timings of the experiments has not been found. Please regenerate it by running convertIndexes2Time.m")
 end
 
-% Select whether to consider the CMC based on IK as reported in Seth et al. (2019)
-% or by considering the improved kinematics in Belli et al. (2023)
-answer = questdlg('IK results to be considered', ...
-	'CMC', ...
-	'IK from 2019 (Seth et al.)','IK refined (Belli et al.)','IK refined (Belli et al.)');
-
-switch answer
-    case 'IK from 2019 (Seth et al.)'
-        original_analysis = 1;
-    case 'IK refined (Belli et al.)'
-        original_analysis = 0;
-end
 
 fileName='ThoracoscapularShoulderCMC_states.sto'; % specifiy the file name of the states file 
 taskNames={'abd01';'abd02';'abd03';'abd21';'abd22';'abd23';'flx01';'flx02';'flx03';'flx21';'flx22';'flx23';...
     'shrug01';'shrug02';'shrug03';'shrug21';'shrug22';'shrug23'};
 
-if original_analysis
-    for i=1:18
-        Dir_file(i)=strcat('../../../../Results/original CMC analysis/',taskNames(i),'/',fileName);
-    end
-else
-    for i=1:18
-        Dir_file(i)=strcat('../../../../Results/new CMC analysis/',taskNames(i),'/',fileName);
-    end 
-end
+for i=1:18
+    Dir_file(i)=strcat('../../../../Results/CMC analysis/',taskNames(i),'/',fileName);
+end 
 
 file=char(Dir_file);
 
@@ -59,11 +41,7 @@ for i=1:18
     fileID=fopen(file(i,:));
     CMC_data=textscan(fileID,format_CMC,'headerlines',7);
     CMC_data=cell2mat(CMC_data);
-    if original_analysis
-        tol = 1e-5;
-    else
-        tol = 3 * (CMC_data(2,1)-CMC_data(1,1));
-    end
+    tol = 3 * (CMC_data(2,1)-CMC_data(1,1));
     for instant=[1,2,3]
         index_selected = find(abs(CMC_data(:, 1)-timings_matrix(i, instant))<tol);
         index_matrix(i, instant) = index_selected(1);
@@ -71,10 +49,6 @@ for i=1:18
     fclose(fileID);
 end
 
-if original_analysis
-    save("index_matrix_CMC_oldStudy", 'index_matrix');
-    fprintf("Old indexes successfully saved in %s \n", pwd)
-else
-    save("index_matrix_CMC_newStudy", 'index_matrix');
-    fprintf("New indexes successfully saved in %s \n", pwd)
-end
+
+save("index_matrix_CMC_newStudy", 'index_matrix');
+fprintf("New indexes successfully saved in %s \n", pwd)
